a2d67c71af98a97c8f49fb59e97f9f49c150f2ea
Unknown <stian678@gmail.com>
2017-08-09 11:40:45 +0200

    Class to test between 5 and 20

---
+++ git Practical/Test5_20.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_Practical
+{
+    class Test5_20
+    {
+        public bool test (int n)
+        {
+            return 5 < n && n < 20;
+        }
+    }
+}
